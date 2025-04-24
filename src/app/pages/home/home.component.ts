237559d41999972aea08f0181e51c67957d0fb80
winvale <harledvalencia01@gmail.com>
2025-04-23 19:47:35 -0500

    Fix SCSS variables and component imports

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ToolsLanguagesComponent } from '../../components/tools-languages/tools-languages.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ToolsLanguagesComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
